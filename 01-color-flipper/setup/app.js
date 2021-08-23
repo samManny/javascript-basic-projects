722a750cba4dfcf7541e0fb67fa996dcdcff59fb
301Dmanny <graphic.vii.studio@gmail.com>
2021-08-23 06:17:08 +0300

    Updates as tutorial goes along I guess ?

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["yellow", "red", "rgba(123,34,98)", "#115d9c"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+
+    //Get a random number btn 0 and 3
+
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
